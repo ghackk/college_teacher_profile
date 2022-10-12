74f80f356b7be5792e650e497460430ac010850d
Gyanesh Kumar <43182259+ghackk@users.noreply.github.com>
2022-10-12 12:02:29 +0530

    local_host and main ipynb file

---
+++ local_host.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Program Files\Google\Chrome\Application"
+.\chrome.exe --remote-debugging-port=6520 --user-data-dir="C:\Users\gyane\OneDrive\Desktop\5\data_science_lab\web automation\local data"
